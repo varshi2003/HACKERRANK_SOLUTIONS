61cba77c5733fca9026622fbfc7598d59779f260
varshi2003 <113238130+varshi2003@users.noreply.github.com>
2023-08-16 14:46:11 +0530

    Create basic_datatypes.cpp

---
+++ basic_datatypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio> 
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a; long b; char c; float d; double e;
+cin>>a>>b>>c>>d>>e;
+cout<<a<<"\n"<<b<<"\n"<<c<<"\n";
+cout<<fixed<<setprecision(6)<<d<<"\n";
+cout<<fixed<<setprecision(14)<<e<<"\n"; 
+return 0;
+    return 0;
+}
